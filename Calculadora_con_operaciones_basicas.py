b5f4222e34584eed94afdbf61328706f67efd493
Nicolas Mendez <nicolath785@gmail.com>
2024-10-25 15:36:43 -0500

    Se crearon las condicionales de cada operacion para que el programa pueda hacer la opracion elejida por el usuario

--- Calculadora_con_operaciones_basicas.py
+++ Calculadora_con_operaciones_basicas.py
@@ -1,8 +1,43 @@
 bucle = True
 while bucle:
-    op = str(input("""Elija la operacion a realizar
-                    A.Suma 
-                    B.Resta
-                    C.Mltiplicacion
-                    D.Division
-                    X.salir"""))
+    op = input("""Elija la operacion a realizar
+    A.Suma 
+    B.Resta
+    C.Multiplicacion
+    D.Division
+    X.salir
+    """).upper()
+
+    if op=="A":
+        print("---Suma---")
+
+        num1 = int(input("Dijite el primer valor a sumar"))
+        num2 = int(input("Dijite el segundo numero a sumar"))
+
+        print(f"El resultado de la suma es: {num1+num2}")
+    elif op=="B":
+        print("---Resta---")
+
+        num3 = int(input("Dijite el primer valor a restar"))
+        num4 = int(input("Dijite el segundo numero a restar"))
+
+        print(f"El resultado de la resta es: {num3-num4}")
+    elif op=="C":
+        print("---Multiplicacion---")
+
+        num5 = int(input("Dijite el primer valor a multiplicar"))
+        num6 = int(input("Dijite el segundo numero a multiplicar"))
+
+        print(f"El resultado de la multiplicacion es: {num5*num6}")
+    elif op=="D":
+        print("---Division---")
+
+        num7 = int(input("Dijite el primer valor a dividir"))
+        num8 = int(input("Dijite el segundo numero a dividir"))
+
+        print(f"El resultado de la division es: {num7*num8}")
+    elif op=="X":
+
+        print("SALIENDO DEL PROGRAMA")
+    else:
+        print("Opcion incorrecta vuelva a intentarlo")
